f8cd0b2e14af02d26e5727c0933e7ef33218f6d8
aslepenkov <alexmichel249@gmail.com>
2020-08-17 12:40:14 +0700

    dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XTStatServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
